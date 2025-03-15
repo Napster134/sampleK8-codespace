f579909f8c72b87bb059e6a257330e91bab1e2ff
Marco <Napster134@hotmail.com>
2025-03-15 21:20:08 +0000

    creating test cluster from flux-local-dev

---
+++ .devcontainer/init.sh
@@ -0,0 +1,17 @@
+git clone https://github.com/stefanprodan/flux-local-dev.git
+cd flux-local-dev
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo >> /home/codespace/.bashrc
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/codespace/.bashrc
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+make tools
+
+nohup kubectl port-forward -n monitoring svc/kube-prometheus-stack-grafana 3000:80 > grafana.log 2>&1 &
+nohup kubectl port-forward -n flux-system svc/weave-gitops 3001:9001 > flux.log 2>&1 &
+nohup kubectl port-forward -n apps svc/podinfo 3002:9898 > podinfo.log 2>&1 &
+
+# To see port-forwarded services: 
+# ps aux | grep kubectl
